1a79320fa36b38cad6a73f597d8769ad6f8ec72a
Qick <77444347+QickTM@users.noreply.github.com>
2021-09-21 16:53:53 +0800

    Add files via upload

---
+++ Chiyung_Miniproject2/LoginForm.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Chiyung_Miniproject2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.RegistrationButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.historyButton = new System.Windows.Forms.Button();
+            this.loginPictureBox = new System.Windows.Forms.PictureBox();
+            this.forgotButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.loginPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.msgLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.msgLabel.ForeColor = System.Drawing.Color.Red;
+            this.msgLabel.Location = new System.Drawing.Point(43, 613);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(699, 51);
+            this.msgLabel.TabIndex = 41;
+            this.msgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.AcceptsReturn = true;
+            this.nameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nameTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.nameTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.nameTextBox.Font = new System.Drawing.Font("Arial", 15F);
+            this.nameTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.nameTextBox.Location = new System.Drawing.Point(235, 498);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameTextBox.MaxLength = 12;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.nameTextBox.Size = new System.Drawing.Size(268, 36);
+            this.nameTextBox.TabIndex = 36;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.SystemColors.Control;
+            this.lblPassword.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblPassword.Font = new System.Drawing.Font("Arial", 15F);
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPassword.Location = new System.Drawing.Point(84, 560);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPassword.Size = new System.Drawing.Size(131, 28);
+            this.lblPassword.TabIndex = 35;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.SystemColors.Control;
+            this.lblName.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblName.Font = new System.Drawing.Font("Arial", 15F);
+            this.lblName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblName.Location = new System.Drawing.Point(84, 501);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblName.Size = new System.Drawing.Size(103, 28);
+            this.lblName.TabIndex = 34;
+            this.lblName.Text = "User ID:";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginButton.BackColor = System.Drawing.SystemColors.Control;
+            this.LoginButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LoginButton.Font = new System.Drawing.Font("Arial", 15F);
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginButton.Location = new System.Drawing.Point(530, 517);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LoginButton.Size = new System.Drawing.Size(191, 55);
+            this.LoginButton.TabIndex = 38;
+            this.LoginButton.Text = "&Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // RegistrationButton
+            // 
+            this.RegistrationButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RegistrationButton.BackColor = System.Drawing.SystemColors.Control;
+            this.RegistrationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegistrationButton.Location = new System.Drawing.Point(43, 689);
+            this.RegistrationButton.Margin = new System.Windows.Forms.Padding(4);
+            this.RegistrationButton.Name = "RegistrationButton";
+            this.RegistrationButton.Size = new System.Drawing.Size(299, 59);
+            this.RegistrationButton.TabIndex = 40;
+            this.RegistrationButton.Text = "Register for New user";
+            this.RegistrationButton.UseVisualStyleBackColor = false;
+            this.RegistrationButton.Click += new System.EventHandler(this.RegistrationButton_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.AcceptsReturn = true;
+            this.passwordTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passwordTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.passwordTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordTextBox.Font = new System.Drawing.Font("Arial", 15F);
+            this.passwordTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.passwordTextBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.passwordTextBox.Location = new System.Drawing.Point(235, 557);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordTextBox.MaxLength = 12;
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.passwordTextBox.Size = new System.Drawing.Size(268, 36);
+            this.passwordTextBox.TabIndex = 37;
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.QuitButton.BackColor = System.Drawing.SystemColors.Control;
+            this.QuitButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.QuitButton.Font = new System.Drawing.Font("Arial", 15F);
+            this.QuitButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.QuitButton.Location = new System.Drawing.Point(624, 738);
+            this.QuitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.QuitButton.Size = new System.Drawing.Size(118, 59);
+            this.QuitButton.TabIndex = 39;
+            this.QuitButton.Text = "&Quit";
+            this.QuitButton.UseVisualStyleBackColor = false;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("Engravers MT", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 124);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Hangman Game Login";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // historyButton
+            // 
+            this.historyButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.historyButton.BackColor = System.Drawing.SystemColors.Control;
+            this.historyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.historyButton.Location = new System.Drawing.Point(395, 738);
+            this.historyButton.Name = "historyButton";
+            this.historyButton.Size = new System.Drawing.Size(191, 59);
+            this.historyButton.TabIndex = 44;
+            this.historyButton.Text = "View Logs";
+            this.historyButton.UseVisualStyleBackColor = false;
+            this.historyButton.Click += new System.EventHandler(this.historyButton_Click);
+            // 
+            // loginPictureBox
+            // 
+            this.loginPictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loginPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.loginPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.loginPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("loginPictureBox.Image")));
+            this.loginPictureBox.Location = new System.Drawing.Point(223, 124);
+            this.loginPictureBox.Name = "loginPictureBox";
+            this.loginPictureBox.Size = new System.Drawing.Size(312, 350);
+            this.loginPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.loginPictureBox.TabIndex = 43;
+            this.loginPictureBox.TabStop = false;
+            // 
+            // forgotButton
+            // 
+            this.forgotButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.forgotButton.BackColor = System.Drawing.SystemColors.Control;
+            this.forgotButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.forgotButton.Font = new System.Drawing.Font("Arial", 15F);
+            this.forgotButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.forgotButton.Location = new System.Drawing.Point(43, 769);
+            this.forgotButton.Margin = new System.Windows.Forms.Padding(4);
+            this.forgotButton.Name = "forgotButton";
+            this.forgotButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.forgotButton.Size = new System.Drawing.Size(299, 55);
+            this.forgotButton.TabIndex = 45;
+            this.forgotButton.Text = "Forgot Password";
+            this.forgotButton.UseVisualStyleBackColor = false;
+            this.forgotButton.Click += new System.EventHandler(this.forgotButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 1003);
+            this.Controls.Add(this.forgotButton);
+            this.Controls.Add(this.historyButton);
+            this.Controls.Add(this.loginPictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.RegistrationButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.QuitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.loginPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msgLabel;
+        public System.Windows.Forms.TextBox nameTextBox;
+        public System.Windows.Forms.Label lblPassword;
+        public System.Windows.Forms.Label lblName;
+        public System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button RegistrationButton;
+        public System.Windows.Forms.TextBox passwordTextBox;
+        public System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox loginPictureBox;
+        private System.Windows.Forms.Button historyButton;
+        public System.Windows.Forms.Button forgotButton;
+    }
+}
---
+++ Chiyung_Miniproject2/LoginForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Chiyung_Miniproject2
+{
+    public partial class LoginForm : Form
+    {
+        public static string strName;
+        public static LoginForm objLogin = new LoginForm();
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearLabels() 
+        {
+            //clear labels
+            msgLabel.Text = "";
+            nameTextBox.Text = "";
+            passwordTextBox.Text = "";
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check for correct User ID, if not then notify user of wrong User ID
+                string strpassword;
+
+                if (nameTextBox.Text == "")
+                {
+                    msgLabel.Text = "*Please enter \"User ID\"!";
+                    nameTextBox.Focus();
+                }
+                else if (passwordTextBox.Text == "")
+                {
+                    msgLabel.Text = "*Please enter \"Password\"!";
+                    passwordTextBox.Focus();
+                }
+                else
+                {
+                    //Search for .txt file with the same User ID entered
+                    StreamReader newUserReader = new StreamReader(nameTextBox.Text + ".txt", true);
+                    newUserReader.ReadLine();
+                    strName = newUserReader.ReadLine();
+                    newUserReader.ReadLine();
+                    strpassword = newUserReader.ReadLine();
+                    newUserReader.Close();
+
+                    if (strpassword == passwordTextBox.Text)
+                    {
+                        //Clear labels
+                        msgLabel.Text = "";
+                        nameTextBox.Text = "";
+                        passwordTextBox.Text = "";
+
+                        //Delay execution of next line of code to simulate processing
+                        Thread.Sleep(500);
+
+                        //Add login time to .txt file
+                        StreamWriter swTime = new StreamWriter(LoginForm.strName + ".txt", true);
+                        DateTime dtNow = DateTime.Now;
+                        swTime.WriteLine("Login time: " + dtNow.ToString());
+                        swTime.Close();
+
+                        //Show game form and hide the login form
+                        objLogin = this;
+                        this.Hide();
+                        TopicForm.objTopic.Show();
+                    }
+                    else
+                    {
+                        msgLabel.Text = "*Login fail, wrong \"Password\"";
+                        passwordTextBox.Focus();
+                    }
+                }
+            }
+            catch
+            {
+                msgLabel.Text = "*User does not exist";
+                nameTextBox.Focus();
+            }
+        }
+
+        private void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            ClearLabels();
+
+            //Hide LoginForm and show RegistrationForm
+            objLogin = this;
+            this.Hide();
+            RegistrationForm.objRegForm.Show();
+        }
+
+        private void historyButton_Click(object sender, EventArgs e)
+        {
+            HistoryForm.objHistory.Show();
+        }
+
+        private void forgotButton_Click(object sender, EventArgs e)
+        {
+            ClearLabels();
+
+            //Hide login and show change password form
+            this.Hide();
+            ChangePasswordForm.objChange.Show();
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
